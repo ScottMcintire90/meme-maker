44c503e99900541ab5c88938d44c14ddfee0b55d
sm <scottmcintire90@gmail.com>
2016-07-01 12:56:28 -0700

    add feature to use camera to take photo

--- app/src/main/java/com/epicodus/mememaker/CameraImageActivity.java
+++ app/src/main/java/com/epicodus/mememaker/CameraImageActivity.java
@@ -1,25 +1,55 @@
 package com.epicodus.mememaker;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class CameraImageActivity extends AppCompatActivity {
-    @Bind(Button mCameraButton;
-    private ImageView mCameraImageView;
+    @Bind(R.id.cameraButton) Button mCameraButton;
+    @Bind(R.id.cameraImage) ImageView mCameraImage;
+    private String mCurrentPhotoPath;
+    private static final int CAMERA_REQUEST = 1888;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_camera_image);
+        ButterKnife.bind(this);
 
-        takePictureButton = (Button) findViewById(R.id.button_image);
-        imageView = (ImageView) findViewById(R.id.imageview);
+        mCameraButton.setOnClickListener(new View.OnClickListener() {
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            takePictureButton.setEnabled(false);
-            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE }, 0);
+            @Override
+            public void onClick(View v) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            mCameraImage.setImageBitmap(photo);
         }
     }
 }
