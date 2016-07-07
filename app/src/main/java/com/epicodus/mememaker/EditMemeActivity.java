05829074c1c5f7013b466747e70cccd6fdca4332
sm <scottmcintire90@gmail.com>
2016-07-07 16:44:29 -0700

    add code for bitmap

--- app/src/main/java/com/epicodus/mememaker/EditMemeActivity.java
+++ app/src/main/java/com/epicodus/mememaker/EditMemeActivity.java
@@ -1,16 +1,11 @@
 package com.epicodus.mememaker;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Rect;
-import android.graphics.Typeface;
-import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,13 +13,19 @@ import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class EditMemeActivity extends AppCompatActivity {
     public static final String TAG = EditMemeActivity.class.getSimpleName();
 
-    @Bind(R.id.editMemeImage) ImageView mEditMemeImage;
+    @Bind(R.id.editMemeImage)
+    ImageView mEditMemeImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,45 +38,29 @@ public class EditMemeActivity extends AppCompatActivity {
         Log.d(TAG, image);
 
         Picasso.with(EditMemeActivity.this).load(image).into(mEditMemeImage);
+
+//        getBitmapFromURL(image);
+//
+//        Bitmap bitmap = getBitmapFromURL(image); // Load your bitmap here
+//        Canvas canvas = new Canvas(bitmap);
+//        Paint paint = new Paint();
+//        paint.setColor(Color.WHITE);
+//        paint.setTextSize(20);
+//        canvas.drawText("Some Text here", 5, 5, paint);
     }
 
-    public Bitmap drawTextToBitmap(Context gContext,
-                                   int gResId,
-                                   String gText) {
-        Resources resources = gContext.getResources();
-        float scale = resources.getDisplayMetrics().density;
-        Bitmap bitmap =
-                BitmapFactory.decodeResource(resources, gResId);
-
-        android.graphics.Bitmap.Config bitmapConfig =
-                bitmap.getConfig();
-        // set default bitmap config if none
-        if(bitmapConfig == null) {
-            bitmapConfig = android.graphics.Bitmap.Config.ARGB_8888;
+    public static Bitmap getBitmapFromURL(String image) {
+        try {
+            URL url = new URL(image);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+            return myBitmap;
+        } catch (IOException e) {
+            // Log exception
+            return null;
         }
-        // resource bitmaps are imutable,
-        // so we need to convert it to mutable one
-        bitmap = bitmap.copy(bitmapConfig, true);
-
-        Canvas canvas = new Canvas(bitmap);
-        // new antialised Paint
-        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        // text color - #3D3D3D
-        paint.setColor(Color.rgb(61, 61, 61));
-        // text size in pixels
-        paint.setTextSize((int) (14 * scale));
-        // text shadow
-        paint.setShadowLayer(1f, 0f, 1f, Color.WHITE);
-
-        // draw text to the Canvas center
-        Rect bounds = new Rect();
-        paint.getTextBounds(gText, 0, gText.length(), bounds);
-        int x = (bitmap.getWidth() - bounds.width())/2;
-        int y = (bitmap.getHeight() + bounds.height())/2;
-
-        canvas.drawText(gText, x, y, paint);
-
-        return bitmap;
     }
-
 }
