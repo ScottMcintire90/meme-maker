f09b2af68d5cb12589d8f1a76fc2e13ee71b140a
sm <scottmcintire90@gmail.com>
2016-07-13 13:17:50 -0700

    add styling to meme

--- app/src/main/java/com/epicodus/mememaker/ui/MemeActivity.java
+++ app/src/main/java/com/epicodus/mememaker/ui/MemeActivity.java
@@ -13,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Layout;
 import android.text.StaticLayout;
+import android.text.TextPaint;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
@@ -62,34 +63,42 @@ public class MemeActivity extends AppCompatActivity {
             Canvas canvas = new Canvas(bitmap);
 
             // new antialised Paint
-            Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+            TextPaint paint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
             // text color - #3D3D3D
             paint.setColor(Color.rgb(255,255,255));
             // text size in pixels
             paint.setTextSize((int) (13 * scale));
             // text shadow
-            paint.setShadowLayer(1f, 0f, 1f, Color.BLACK);
+            paint.setShadowLayer(10, 1, 1, Color.BLACK);
 
-//            //set text width to canvas with minus 16dp padding
-//            int mTextWidth = canvas.getWidth() - (int) (16 * scale);
-//
-//            //init static layout for text
-//            StaticLayout mTextLayout = new StaticLayout(mText,
-//                    paint, mTextWidth, Layout.Alignment.ALIGN_CENTER, 1.0f, 0.0f, false);)
+            //set text width to canvas with minus 16dp padding
+            int mTextWidth = canvas.getWidth() - (int) (0 * scale);
+            int yTextWidth = canvas.getWidth() - (int) (0 * scale);
+            //init static layout for text
+            StaticLayout mTextLayout = new StaticLayout(mText,
+                    paint, mTextWidth, Layout.Alignment.ALIGN_CENTER, 1.0f, 0.0f, false);
+            StaticLayout yTextLayout = new StaticLayout(yText,
+                    paint, yTextWidth, Layout.Alignment.ALIGN_CENTER, 1.0f, 0.0f, false);
+            // get height of multiline text
+            int mTextHeight = mTextLayout.getHeight();
+
+            int yTextHeight = yTextLayout.getHeight();
+            // get position of text's top left corner
+            float x = (bitmap.getWidth() - mTextWidth)/2;
+            float y = (bitmap.getHeight() - mTextHeight)/250;
+
+            // get position of text's top left corner
+            float q = (bitmap.getWidth() - yTextWidth)/2;
+            float z = (bitmap.getHeight() - yTextHeight)/1;
 
             // draw text to the Canvas center
-            Rect bounds = new Rect();
-            paint.getTextBounds(mText, 0, mText.length(), bounds);
-            int x = (bitmap.getWidth() - bounds.width())/7;
-            int y = (bitmap.getHeight() + bounds.height())/23;
-
-            canvas.drawText(mText, x * scale, y * scale, paint);
-
-            paint.getTextBounds(yText, 0, yText.length(), bounds);
-            int z = (bitmap.getWidth() - bounds.width())/7;
-            int q = (bitmap.getHeight() + bounds.height())/4;
-
-            canvas.drawText(yText, z * scale, q * scale, paint);
+            canvas.save();
+            canvas.translate(x, y);
+            mTextLayout.draw(canvas);
+            canvas.save();
+            canvas.translate(q, z);
+            yTextLayout.draw(canvas);
+            canvas.restore();
 
             return bitmap;
         } catch (Exception e) {
@@ -102,3 +111,5 @@ public class MemeActivity extends AppCompatActivity {
 }
 
 //http://www.skoumal.net/en/android-drawing-multiline-text-on-bitmap/
+
+//impact font and all caps
