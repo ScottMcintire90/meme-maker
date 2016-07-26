b74f361f233e7cc1fd9b3bff3ff6e73df971f055
sm <scottmcintire90@gmail.com>
2016-07-26 08:45:40 -0700

    add code to setup firebase adapter on user account page

--- app/src/main/java/com/epicodus/mememaker/ui/UserAccountActivity.java
+++ app/src/main/java/com/epicodus/mememaker/ui/UserAccountActivity.java
@@ -2,28 +2,59 @@ package com.epicodus.mememaker.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import com.epicodus.mememaker.R;
+import com.epicodus.mememaker.adapters.FirebaseMemeViewHolder;
 import com.epicodus.mememaker.extensions.BaseActivity;
+import com.epicodus.mememaker.models.MemeUrl;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
 import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class UserAccountActivity extends BaseActivity {
-    private UserAccountActivity mAdapter2;
-    @Bind(R.id.recyclerView) RecyclerView mRecyclerView2;
-    private DatabaseReference mRef;
+    private DatabaseReference mMemeReference;
+    private FirebaseRecyclerAdapter mFirebaseAdapter;
+
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_account);
+        ButterKnife.bind(this);
+
+        mMemeReference = FirebaseDatabase.getInstance().getReference("Url");
+        setUpFirebaseAdapter();
     }
 
+    private void setUpFirebaseAdapter() {
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<MemeUrl, FirebaseMemeViewHolder>
+                (MemeUrl.class, R.layout.meme_list_item, FirebaseMemeViewHolder.class,
+                        mMemeReference) {
 
+            @Override
+            protected void populateViewHolder(FirebaseMemeViewHolder viewHolder,
+                                              MemeUrl model, int position) {
+                viewHolder.bindRestaurant(model);
+            }
+        };
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mFirebaseAdapter);
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mFirebaseAdapter.cleanup();
+    }
+
 }
